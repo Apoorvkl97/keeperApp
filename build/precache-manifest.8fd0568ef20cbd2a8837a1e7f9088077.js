01fb229588637d24676cd50cab72f0524a65996d
Apoorvkl97 <97307629+Apoorvkl97@users.noreply.github.com>
2022-01-07 23:21:20 +0530

    Add files via upload

---
+++ build/precache-manifest.8fd0568ef20cbd2a8837a1e7f9088077.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345821007a7920e5e262b76804cc3dde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdbbed2bf7928a43be96",
+    "url": "/static/js/2.fabae278.chunk.js"
+  },
+  {
+    "revision": "a9941c2723d31b3b4250",
+    "url": "/static/js/main.57aa456a.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
